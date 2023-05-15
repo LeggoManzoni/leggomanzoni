f38c9f7b002e214dbf9a05b32432958a436f68d2
GiuliaMenna <72873975+GiuliaMenna@users.noreply.github.com>
2023-05-15 17:53:37 +0200

    aggiunte partials e varie

--- routes/reader.js
+++ routes/reader.js
@@ -7,7 +7,7 @@ const router = express.Router();
 /* reader */
 router.get("/reader", (req, res) => {
     //res.render("reader");
-    var chapters = ["1", "2", "3", "4", "5", "6"]; // da estrarre dinamicamente dai file XML
+    var chapters = ["Frontespizio", "Introduzione", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38"]; // da estrarre dinamicamente dai file XML
     res.render("reader", {
         chapters: chapters
     });
