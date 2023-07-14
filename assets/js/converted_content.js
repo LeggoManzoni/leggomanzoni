a02ecaae434d691067af2e52754bea9bc7425d59
@mary-lev <marylevchenko@gmail.com>
2023-07-14 03:47:10 +0200

    Change comments visualization

--- assets/js/converted_content.js
+++ assets/js/converted_content.js
@@ -1,123 +1,70 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Constants and variables
-
     const defaultComment = 'Luca Badini Confalonieri';
     const defaultChapter = 'intro';
-    let secondDefaultComment;
 
-    // Fetch and display the default comment
-    fetch(`/get-comment/${defaultComment}`)
+    // Fetch and display the default comment and chapter
+    fetchAndDisplayData(`/get-comment/${defaultComment}`, '.text-comment-top', 'data-active-comment');
+    fetchAndDisplayData(`/get-chapter/${defaultChapter}`, '.text-chapter');
+
+    // Setup MutationObserver
+    setupMutationObserver('.divisione');
+
+    // Setup click event listeners for links
+    setupLinkClickListener('.chapter-link', fetchAndDisplayData, '.text-chapter');
+    setupLinkClickListener('.comment-link', fetchAndDisplayData, '.text-comment-top');
+    setupLinkClickListener('.comment-link-2', fetchAndDisplayData, '.text-comment-bottom');
+
+    setupHoverScrolling();
+});
+
+function fetchAndDisplayData(endpoint, selector, attribute) {
+    fetch(endpoint)
         .then(response => response.text())
         .then(data => {
-            const commentTopElement = document.querySelector('.text-comment-top');
-            if (commentTopElement) {
-                commentTopElement.innerHTML = data;
-                commentTopElement.setAttribute('data-active-comment', defaultComment);
+            const element = document.querySelector(selector);
+            if (element) {
+                element.innerHTML = data;
+                element.setAttribute(attribute, data);
             }
         })
+        .then(removeHighlightFromHoverItems())
         .catch(console.error);
+}
 
-    // Fetch and display the default chapter
-    fetch(`/get-chapter/${defaultChapter}`)
-        .then(response => response.text())
-        .then(data => {
-            const chapterElement = document.querySelector('.text-chapter');
-            if (chapterElement) {
-                chapterElement.innerHTML = data;
-            }
-        })
-        .catch(console.error);
-
-    // Mutation Observer setup
-    const targetNode = document.querySelector('.divisione');
+function setupMutationObserver(selector) {
+    const targetNode = document.querySelector(selector);
     if (targetNode) {
         const observer = new MutationObserver((mutationsList) => {
             for (const mutation of mutationsList) {
                 if (mutation.type === 'attributes' && mutation.attributeName === 'class') {
-                    secondDefaultComment = hasSingularTextClass(mutation.target) ? '' : 'Angelini';
-
-                    fetch(`/get-comment/${secondDefaultComment}`)
-                        .then(response => response.text())
-                        .then(data => {
-                            const commentBottomElement = document.querySelector('.text-comment-bottom');
-                            if (commentBottomElement) {
-                                commentBottomElement.innerHTML = data;
-                                commentBottomElement.setAttribute('data-active-comment', secondDefaultComment);
-                            }
-                        })
-                        .catch(console.error);
+                    const comment = hasSingularTextClass(mutation.target) ? '' : 'Angelini';
+                    fetchAndDisplayData(`/get-comment/${comment}`, '.text-comment-bottom', 'data-active-comment');
                 }
             }
         });
         observer.observe(targetNode, { attributes: true, attributeFilter: ['class'] });
     }
+}
 
-    // Event listeners for chapter links
-    document.querySelectorAll('.chapter-link').forEach(link => {
+function setupLinkClickListener(selector, fetchAndDisplayFunction, displaySelector) {
+    document.querySelectorAll(selector).forEach(link => {
         link.addEventListener('click', event => {
             event.preventDefault();
-            const chapter = event.target.getAttribute('data-chapter');
-            fetch(`/get-chapter/${chapter}`)
-                .then(response => response.text())
-                .then(data => {
-                    const chapterElement = document.querySelector('.text-chapter');
-                    if (chapterElement) {
-                        chapterElement.innerHTML = data;
-                    }
-                })
-                .catch(console.error);
+            const data = event.target.getAttribute('data-comment');
+            fetchAndDisplayFunction(`/get-comment/${data}`, displaySelector, 'data-active-comment');
         });
     });
+}
 
-    // Event listeners for comment links
-    document.querySelectorAll('.comment-link').forEach(link => {
-        link.addEventListener('click', event => {
-            event.preventDefault();
-            const comment = event.target.getAttribute('data-comment');
-            const secondDivActiveComment = document.querySelector('.text-comment-bottom').getAttribute('data-active-comment');
-            if (secondDivActiveComment === comment) {
-                alert('Il commento selezionato è già attivo nel secondo specchietto dedicato al commento. Scegline un altro per fare un confronto.');
-                return;
-            }
-            fetch(`/get-comment/${comment}`)
-                .then(response => response.text())
-                .then(data => {
-                    const commentTopElement = document.querySelector('.text-comment-top');
-                    if (commentTopElement) {
-                        commentTopElement.innerHTML = data;
-                        commentTopElement.setAttribute('data-active-comment', comment);
-                    }
-                })
-                .catch(console.error);
-        });
+function removeHighlightFromHoverItems() {
+    const hoverItems = Array.from(document.querySelectorAll('.hover-item'));
+
+    // For each hover-item, remove the highlight class
+    hoverItems.forEach(hoverItem => {
+        hoverItem.classList.remove('highlight');
     });
-
-    // Event listeners for comment links in second div
-    document.querySelectorAll('.comment-link-2').forEach(link => {
-        link.addEventListener('click', event => {
-            event.preventDefault();
-            const comment = event.target.getAttribute('data-comment');
-            const firstDivActiveComment = document.querySelector('.text-comment-top').getAttribute('data-active-comment');
-            if (firstDivActiveComment === comment) {
-                alert('Il commento selezionato è già attivo nel primo specchietto dedicato al commento. Scegline un altro per fare un confronto.');
-                return;
-            }
-            fetch(`/get-comment/${comment}`)
-                .then(response => response.text())
-                .then(data => {
-                    const commentBottomElement = document.querySelector('.text-comment-bottom');
-                    if (commentBottomElement) {
-                        commentBottomElement.innerHTML = data;
-                        commentBottomElement.setAttribute('data-active-comment', comment);
-                    }
-                })
-                .catch(console.error);
-        });
-    });
-
-    setupHoverScrolling();
-
-});
+}
 
 
 // Utility function to check if the target element has the specified classes
@@ -125,54 +72,51 @@ function hasSingularTextClass(target) {
     return target.classList.contains('divisione') && target.classList.contains('singularText');
 }
 
+function highlightHoveredItem() {
+    const hoverItems = Array.from(document.querySelectorAll('.hover-item'));
+    const scrollItems = Array.from(document.querySelectorAll('.scroll-item'));
+    hoverItems.forEach(hoverItem => {
+        const hoverItemId = hoverItem.getAttribute('data-id');
+        const correspondingScrollItem = scrollItems.find(scrollItem => scrollItem.getAttribute('data-related-id') === hoverItemId);
+
+        // If a corresponding scroll-item exists, toggle the highlight class
+        if (correspondingScrollItem) {
+            hoverItem.classList.toggle('highlight');
+        }
+    });
+}
+
 function setupHoverScrolling() {
-    var highlightedElements = []; // Store the previously highlighted element
-    document.addEventListener('mouseover', function (event) {
+    const commentsContainer = document.getElementById('upperDiv');
+    document.addEventListener('click', function (event) {
         // Check if the clicked element has the class "hover-item"
         if (event.target.classList.contains('hover-item')) {
             // Get the data-id of the clicked element
-            var dataId = event.target.getAttribute('data-id');
+            const dataId = event.target.getAttribute('data-id');
 
             // Find the corresponding element in the right column by data-related-id
-            var elements = document.querySelectorAll('.scroll-item');
-            // var correspondingElement = document.querySelector('.scroll-item[data-related-id="' + dataId + '"]');
-            for (var i = 0; i < elements.length; i++) {
-                var currentId = parseInt(elements[i].getAttribute('data-related-id'), 10);
+            let correspondingElement;
+            const elements = Array.from(document.querySelectorAll('.scroll-item'));
+            for (let i = 0; i < elements.length; i++) {
+                const currentId = parseInt(elements[i].getAttribute('data-related-id'), 10);
                 if (currentId >= dataId) {
                     correspondingElement = elements[i];
                     break;
                 }
             }
-            
 
             // Scroll the corresponding element into view in the right column
             if (correspondingElement) {
-                for (var j = 0; j < highlightedElements.length; j++) {
-                    highlightedElements[j].classList.remove('highlight');
+                // Calculate the top position of the corresponding element relative to the comments container
+                const correspondingElementTop = correspondingElement.offsetTop - commentsContainer.offsetTop;
+
+                // Scroll the corresponding element into view in the right column
+                commentsContainer.scrollTop = correspondingElementTop;
+                for (let i = 0; i < elements.length; i++) {
+                    elements[i].classList.remove('highlight-comment');
                 }
-                highlightedElements = [];
-
-                var relatedId = parseInt(correspondingElement.getAttribute('data-related-id'), 10);
-                var endId = parseInt(correspondingElement.getAttribute('data-end-id'), 10);
-
-                var hoverItems = document.querySelectorAll('.hover-item');
-                for (var i = 0; i < hoverItems.length; i++) {
-                    var currentId = parseInt(hoverItems[i].getAttribute('data-id'), 10);
-                    if (currentId >= relatedId && currentId <= endId) {
-                        highlightedElements.push(hoverItems[i]);
-                }
+                correspondingElement.classList.add('highlight-comment');
             }
-
-            for (var k = 0; k < highlightedElements.length; k++) {
-                highlightedElements[k].classList.add('highlight');
-            }
-                
-                setTimeout(() => {
-                    correspondingElement.scrollIntoView({ behavior: 'smooth' , block: 'start', inline: 'nearest'});
-                }, 100); // 100 ms delay
-            }
-
         }
     });
 }
-
